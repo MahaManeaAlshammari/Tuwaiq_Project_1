0316770657811aa583ac473f8fc68baa257dbb9e
mahaManae <johndoe@example.com>
2021-10-21 16:49:22 +0300

    projct-1

--- main.js
+++ main.js
@@ -1,12 +1,67 @@
+const Name = $("#name");
+const Img = $("#img");
+const Price = $("#price");
+const Div = $("#div");
+const DivX = $("#divX");
 
-
-function hideHandler() {
-   const hide=(data); 
-  mainContainer.hide();
+const FoodArray =[
+  {NameFood : "خبز " , ImgFood: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBU782kuclKH7VYqPBhAQCYLjVvL5gikr7iw&usqp=CAU" , priceFood: "3 RS"} ,
+  {NameFood : " مشروب صحي" , ImgFood: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfX9tlh7bQXYnOPo1kbLW78F0CirmGUSORAg&usqp=CAU" , priceFood: "9RS"},
+  {NameFood : " سناك" , ImgFood: "https://encrypted-tbn0.gstatic.com/images?  q=tbn:ANd9GcT0SkxuKH9hUYn-r2ueQOA6ZR2BvGCKtFBE8g&usqp=CAU" , priceFood: "12 RS"},
+  
+  
+];
+function renderArr(array) {
+  for (let i = 0; i < array.length; i++) {
+    const div =
+     $(`<div">
+        <img src="${array[i].ImgFood}" >
+        <button id="del" onclick="deleteFoodAray (${i})">حذف</button>
+        <h2>${array[i].NameFood}</h1>
+        <p>${array[i].priceFood}</p>
+        </div>`); 
+Div.append(div);
+    
+  }
 }
+renderArr(FoodArray);
 
+function addFood() {
+  const newFood = {
+      NameFood: Name.val(),
+      ImgFood: Img.val(),
+      priceFood: Price.val(),
+  };
+  FoodArray.push(newFood);
+  renderArr([newFood]);
+}
+// Div.html("")
+// renderArr(FoodArray); 
+
+DivX.hide();
 function showHandler() {
- .show() 
-  mainContainer.show();
+  DivX.show();
 }
+// function hideHandler() {
+//   DivX.hide();
+// }
+
+function deleteFoodAray(indexx){
+FoodArray.splice(indexx,1);
+Div.html("");
+ renderArr(FoodArray); 
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
